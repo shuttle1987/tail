aaaf1657694f8aab0f5c58a74c72a340de08c1f6
Janis Lesinskis <footbagfreestyle@gmail.com>
2017-09-18 07:28:35 +0000

    change package specification

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='python-tail',
     description='Python implementation of UNIX tail command',
     version='0.1',
-    packages=find_packages(),
+    packages=['tail'],
     install_requires=[
         'Click',
     ],
