e8a5a97ea18120915dba74b9a73fdca4eb381568
Janis Lesinskis <footbagfreestyle@gmail.com>
2017-09-19 19:05:32 +0000

    Fix indentation level

--- tail/tests/test_tail.py
+++ tail/tests/test_tail.py
@@ -30,7 +30,7 @@ F
 
     # We need to patch the open found in the namespace of the module
     # where the function is defined
-    with patch('builtins.open', mocked_open, create=True) as mocked_file_open:
+    with patch('tail.open', mocked_open, create=True) as mocked_file_open:
 
         # We also need to patch the file checking because we are not dealing
         # with an actual file in the filesystem in this unit test
@@ -43,40 +43,40 @@ F
     assert res == ["D", "E", "F"]
 
 
-    def test_head_from_file():
-        """Tests that tail works as advertised from a file"""
+def test_head_from_file():
+    """Tests that tail works as advertised from a file"""
 
-        from unittest.mock import mock_open, patch, Mock
+    from unittest.mock import mock_open, patch, Mock
 
-        # The mock_data we are using for our test
-        mock_data = """A
-    B
-    C
-    D
-    E
-    F
-    """
-        mocked_open = mock_open(read_data=mock_data)
+    # The mock_data we are using for our test
+    mock_data = """A
+B
+C
+D
+E
+F
+"""
+    mocked_open = mock_open(read_data=mock_data)
 
-        # mock_open does not support iteration by lines by default so
-        # we must define the following:
-        mocked_open.return_value.__iter__.return_value = mock_data.splitlines()
+    # mock_open does not support iteration by lines by default so
+    # we must define the following:
+    mocked_open.return_value.__iter__.return_value = mock_data.splitlines()
 
 
-        # The file check in the class returns no value upon a valid file
-        # the error states just raise exceptions.
-        mocked_file_validity_check = Mock()
+    # The file check in the class returns no value upon a valid file
+    # the error states just raise exceptions.
+    mocked_file_validity_check = Mock()
 
-        # We need to patch the open found in the namespace of the module
-        # where the function is defined
-        with patch('builtins.open', mocked_open, create=True) as mocked_file_open:
+    # We need to patch the open found in the namespace of the module
+    # where the function is defined
+    with patch('tail.open', mocked_open, create=True) as mocked_file_open:
 
-            # We also need to patch the file checking because we are not dealing
-            # with an actual file in the filesystem in this unit test
-            with patch('tail.tail.check_file_validity', mocked_file_validity_check):
-                res = FileBasedTail('Test_filename.txt').head(3)
+        # We also need to patch the file checking because we are not dealing
+        # with an actual file in the filesystem in this unit test
+        with patch('tail.tail.check_file_validity', mocked_file_validity_check):
+            res = FileBasedTail('Test_filename.txt').head(3)
 
-        mocked_file_validity_check.assert_called_once_with('Test_filename.txt')
-        mocked_file_open.assert_called_once_with('Test_filename.txt', 'r')
-        assert len(res) == 3
-        assert res == ["A", "B", "C"]
+    mocked_file_validity_check.assert_called_once_with('Test_filename.txt')
+    mocked_file_open.assert_called_once_with('Test_filename.txt', 'r')
+    assert len(res) == 3
+    assert res == ["A", "B", "C"]
